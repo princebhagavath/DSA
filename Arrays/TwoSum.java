183f9d60e669bab358014d97e1bfd0f61f4d8783
princebhagavath <murarivb777@gmail.com>
2022-04-20 22:14:16 +0530

    Two Sum

--- Arrays/TwoSum.java
+++ Arrays/TwoSum.java
@@ -1,5 +1,12 @@
-public class TwoSum {
-    public static void main(String[] args) {
-
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (hm.containsKey(comp))
+                return new int[] { hm.get(comp), i };
+            hm.put(nums[i], i);
+        }
+        return null;
     }
 }
